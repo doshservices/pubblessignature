3be58d379128cc5bd8b457d9040309c589b1e3d8
samuelAzurestech <abiokorigho.samuel@azurestech.com>
2021-08-02 12:05:21 +0100

    Update Redis

--- src/service/Redis.js
+++ src/service/Redis.js
@@ -1,14 +1,14 @@
-const redis = require('redis');
+const redis = require("redis");
 const { promisify } = require("util");
-const {REDIS_PORT, OTP_DURATION} = require('../core/config');
+const { REDIS_URL, OTP_DURATION } = require("../core/config");
 
-const redisClient = redis.createClient(REDIS_PORT);
+const redisClient = redis.createClient(REDIS_URL);
 const getAsync = promisify(redisClient.get).bind(redisClient);
 
 exports.cacheData = (key, value, expirationTime = OTP_DURATION) => {
-    redisClient.setex(key, expirationTime, value);
-}
+  redisClient.setex(key, expirationTime, value);
+};
 
 exports.getCachedData = async (key) => {
-    return await getAsync(key);
-}
+  return await getAsync(key);
+};
